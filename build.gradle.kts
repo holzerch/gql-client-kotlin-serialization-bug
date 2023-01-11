fb9f54ffa79b06d9f8d4784d29bc65dc95620122
Christian Holzer <christian.holzer@holzerc.net>
2023-01-11 11:54:31 +0100

    added client generation

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,10 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "3.0.1"
     id("io.spring.dependency-management") version "1.1.0"
-    kotlin("jvm") version "1.7.22"
-    kotlin("plugin.spring") version "1.7.22"
+    id("com.expediagroup.graphql") version "7.0.0-alpha.0"
+    kotlin("jvm") version "1.7.21"
+    kotlin("plugin.spring") version "1.7.21"
+    kotlin("plugin.serialization") version "1.7.21"
 }
 
 group = "gql.client.bug"
@@ -19,7 +21,23 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    implementation("com.expediagroup:graphql-kotlin-spring-client:7.0.0-alpha.0") {
+        exclude("com.expediagroup", "graphql-kotlin-client-jackson")
+    }
+    implementation("com.expediagroup:graphql-kotlin-client-serialization:7.0.0-alpha.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+graphql {
+    client {
+        packageName = "gql.client.bug"
+        schemaFile = file("${project.projectDir}/src/main/resources/gql/test-schema.graphql")
+        queryFileDirectory = "${project.projectDir}/src/main/resources/gql/queries"
+        serializer = com.expediagroup.graphql.plugin.gradle.config.GraphQLSerializer.KOTLINX
+    }
 }
 
 tasks.withType<KotlinCompile> {
