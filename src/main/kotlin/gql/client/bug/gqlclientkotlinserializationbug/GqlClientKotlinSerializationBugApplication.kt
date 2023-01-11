0c595f1a0088e05104f2bcf71de88014800d28cd
Christian Holzer <christian.holzer@holzerc.net>
2023-01-11 09:42:46 +0100

    Initial commit

---
+++ src/main/kotlin/gql/client/bug/gqlclientkotlinserializationbug/GqlClientKotlinSerializationBugApplication.kt
@@ -0,0 +1,11 @@
+package gql.client.bug.gqlclientkotlinserializationbug
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GqlClientKotlinSerializationBugApplication
+
+fun main(args: Array<String>) {
+    runApplication<GqlClientKotlinSerializationBugApplication>(*args)
+}
